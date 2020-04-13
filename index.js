6c76b095c9bef4e72e931cee7ff9fe5fa2d7022f
opikarief <opikarief.id@gmail.com>
2020-04-13 17:25:02 +0700

    modified:   api/presence.js 	modified:   index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = 5000;
-
+var presence = require('./api/presence')
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => {
   console.log(`Server is booming on port 5000
 Visit http://localhost:5000`);
+// presence();
 });
 
 app.get("/", (req, res) => {
