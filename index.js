69e524efbe6227bfd417f0f971f39ab217aee751
opikarief <opikarief.id@gmail.com>
2020-04-13 14:35:37 +0700

    new file:   index.js 	modified:   package-lock.json 	modified:   package.json

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Listen on port 5000
+app.listen(port, () => {
+  console.log(`Server is booming on port 5000
+Visit http://localhost:5000`);
+});
+
+// Import Dependencies
+
+//Specify port
+
+// Body parser
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("Discord Bot NekonyAn is online!");
+  });
+
+const greeting = require('./api/greeting');
+const presence = require('./api/presence');
+const sentences = require('./api/sentences');
+const index = require('./api/index');
+  // Mock APIs
+//   app.get("/users", (req, res) => {
+//     res.json([
+//       { name: "William", location: "Abu Dhabi" },
+//       { name: "Chris", location: "Vegas" }
+//     ]);
+//   });
+  
+//   app.post("/user", (req, res) => {
+//     const { name, location } = req.body;
+  
+//     res.send({ status: "User created", name, location });
+//   });
